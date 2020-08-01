588885de0cee4a2d90dbb6a2b12db7ac166d140d
Daniel Bou <danielbou@outlook.com.br>
2020-08-01 18:50:01 -0300

    A condição elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,7 +8,14 @@ chute = int(input("Digite o seu número: "))
 
 print("Você digitou", chute)
 
-if (chute == numero_secreto):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+
+if (acertou):
     print("Você acertou!")
+elif (maior):
+    print("Você errou! O seu chute foi maior que o número secreto.")
 else:
-    print("Você errou!")
+    print("Você errou! O seu chute foi menor que o número secreto.")
+
+print('Fim do jogo')
