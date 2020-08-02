1d2bb21f24ae7bab84eb7523a0440651b6d3ee67
Daniel Bou <danielbou@outlook.com.br>
2020-08-01 22:28:56 -0300

    Pontuação

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,7 +5,23 @@ print("Bem-vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
+
+print("""Qual nível de dificuldade?
+(1) Fácil 
+(2) Médio
+(3) Difícil
+""")
+
+nivel = int(input(">> "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1,total_de_tentativas+1):
     print("Tentativas: {} de {} ".format(rodada, total_de_tentativas))
@@ -17,10 +33,14 @@ for rodada in range(1,total_de_tentativas+1):
 
     if (acertou):
         print("Você acertou!")
+        print("Total de pontos: ", pontos)
         break
-    elif (maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
     else:
-        print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = numero_secreto - chute
+        pontos -= abs(pontos_perdidos)
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        else:
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-print('Fim do jogo :( O número secreto era o ', numero_secreto)
+print('Fim do jogo. O número secreto era o ', numero_secreto)
