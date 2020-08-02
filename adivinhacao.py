0c7aeb1e03c773de0e820f53d322fff3504aa7cb
Daniel Bou <danielbou@outlook.com.br>
2020-08-01 22:15:14 -0300

    Gerando número aleatório

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,13 +1,15 @@
+import random
+
 print("*********************************")
 print("Bem-vindo no jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
 for rodada in range(1,total_de_tentativas+1):
     print("Tentativas: {} de {} ".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 0 e 100: "))
     print("Você digitou", chute)
 
     acertou = chute == numero_secreto
@@ -21,4 +23,4 @@ for rodada in range(1,total_de_tentativas+1):
     else:
         print("Você errou! O seu chute foi menor que o número secreto.")
 
-print('Fim do jogo')
+print('Fim do jogo :( O número secreto era o ', numero_secreto)
